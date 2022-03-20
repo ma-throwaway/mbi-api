5c3f9b415defb4925d97d181b82c98418c24f148
Maria Altebarmakian <m.altebarmakian@gmail.com>
2022-03-20 18:24:45 -0400

    Convert lowercase chars input by user to uppercase to test MBI format

--- routes/verify.js
+++ routes/verify.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post("/", function(req, res, next) {
   const mbi = req.body.mbi;
   const re = /^[1-9][AC-HJKMNP-RT-Y]([AC-HJKMNP-RT-Y]|[0-9])[0-9][AC-HJKMNP-RT-Y]([AC-HJKMNP-RT-Y]|[0-9])[0-9][AC-HJKMNP-RT-Y][AC-HJKMNP-RT-Y][0-9][0-9]$/
-  const verification = re.test(mbi);
+  const verification = re.test(mbi.toUpperCase());
 
   res.end(verification.toString());
 });
